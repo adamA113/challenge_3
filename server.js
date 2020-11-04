e63fb9d6eadd754e04d3478a831e372afab02d57
Yasir Tobbileh <yasur96@gmail.com>
2020-11-04 11:18:38 +0200

    defining express server

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
+var express = require('express');
+var app = express();
+const fs = require('fs');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static(__dirname + '/client'));
+
+app.post('/', function (req, res) {
+    // console.log(req)
+    
+    
+    res.end();
+});
+
+app.get('/', function (req, res) {
+    res.render("/");
+});
+
+app.listen(3000, () => {
+    console.log('server is listening ')
+})
 
