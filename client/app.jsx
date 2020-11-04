a21f46208559434e0b09b9ab484465ca75e8e9c4
Yasir Tobbileh <yasur96@gmail.com>
2020-11-04 14:57:29 +0200

    react

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,31 @@
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+        
+//         this.state = {
 
+//         };
+//     }
 
+//     render(){
+//         return(
+//             <div>
+//                 <form id="F1">
+//                     <label for="fname">First name:</label><br>
+//                     <input type="text" id="fname" name="fname"><br>
+
+//                     <label for="lname">Last name:</label><br>
+//                     <input type="text" id="lname" name="lname">
+
+//                     <label for="email">Email:</label><br>
+//                     <input type="email" id="email" name="email">
+
+//                     <label for="lname">Password:</label><br>
+//                     <input type="password" id="password" name="password">
+//                 </form>
+//             </div>
+//         )
+//      }
+//  }
+
+// export default App;
---
+++ client/index.js
@@ -0,0 +1,6 @@
+// import App from './client/app.js';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// );
